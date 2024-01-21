a005af3d715960acb339c7b914c5eb08fc697f00
Задорин Артем <artfan95@gmail.com>
2024-01-21 22:49:20 +0700

    Готоы стили и раскрытие списка брендов через js

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+
+let brandsMoreBtn = document.querySelector('.js-brand-more');
+brandsMoreBtn.addEventListener('click', function (){this.closest('.brand-list').classList.toggle('brand-list--open')});
+
