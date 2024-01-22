d86ffb67ce9a91cfb7847a7211b5dbe98c7ff443
Задорин Артем <artfan95@gmail.com>
2024-01-22 21:53:59 +0700

    Доработаны стили, настроен скрипт для слайдера

--- app/js/main.js
+++ app/js/main.js
@@ -2,3 +2,23 @@
 let brandsMoreBtn = document.querySelector('.js-brand-more');
 brandsMoreBtn.addEventListener('click', function (){this.closest('.brand-list').classList.toggle('brand-list--open')});
 
+(function() {
+    if (window.innerWidth < 768){
+        console.log(window)
+        const swiper = new Swiper('.js-brand-slider', {
+            speed: 400,
+            spaceBetween: 16,
+            centeredSlides: true,
+            centerInsufficientSlides: true,
+            slidePerView: 0.5,
+            pagination: {
+                el: '.js-brand-slider-nav',
+                type: 'bullets',
+                bulletActiveClass: 'brand-nav__btn--active',
+                bulletClass: 'brand-nav__btn',
+                bulletElement: 'button',
+                clickable: true
+            }
+        });
+    }
+}())
